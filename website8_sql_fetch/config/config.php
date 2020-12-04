d948d03886f83d727bca8f4852b733b7847cb3ea
HARSH JAIN <jainharsh270@gmail.com>
2020-12-04 22:27:15 +0530

    SQL Fetch

---
+++ website8_sql_fetch/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/phpTut/website8_sql_fetch/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'phpblog');
